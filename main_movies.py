e31ad18c0ebe2af28d812a90fd1c0e6d20d7ef3b
marius <marius.urs@gmail.com>
2014-07-02 15:25:14 +0200

    Styling: singular listing progress, now can display loop

--- main_movies.py
+++ main_movies.py
@@ -78,7 +78,7 @@ class MovieHandler(webapp2.RequestHandler):
 
 class HomePage(MovieHandler):
   def get(self):
-        q = models.MovieListing.gql(("WHERE IMDB_link =:link"), link = "http://getbootstrap.com/css/")
+        q = models.MovieListing.gql("Where Followed= :one", one=1)
         p = list(q)
         logging.error("list(q)=%s"%p)
         self.render("front.html", listing = p)
